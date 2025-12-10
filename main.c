07f06c4939eeedf41d217ac4d3e536861e7c98d4
Karthik-Sree <karthikamalapurapu@gmail.com>
2025-12-10 16:11:37 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,74 @@
+main.c
+/*
+this is the main.c file for execution 
+*/
+#include "stm32f4xx.h"
+#include "IUSART1.h"
+void USART1_echo(char *input);
+void delay(volatile uint32_t t)
+{
+    while(t--);
+}
+volatile uint8_t e=0;
+volatile uint8_t c ;
+uint8_t rx_buf[32];
+uint8_t rx_index=0;
+
+void USART1_echo(char *input )
+{
+	char *p;
+	char expected_response[]="L1 en";
+	char valid_response[]="received\r\n";
+	char invalid_response[]="Command not identified\r\n";
+	if (  (strcmp(input,expected_response))==0 )
+	{
+		p= valid_response;
+		e=10;
+		while(e--)
+		{
+		GPIOA->BSRR = ( 1 << 2)
+		delay(300000);
+		GPIOA->BSRR = (1 <<( 16 + 2 ) );
+				delay(300000);
+		}
+	}
+	else{
+		p = invalid_response;
+	}
+	while(*p)
+	{
+		while( !(USART1->SR & USART_SR_TXE) );
+			USART1->DR = *p++;
+	}
+}
+void USART1_IRQHandler(void)
+{
+	if(USART1->SR & USART_SR_RXNE)
+	{
+		c= USART1->DR;
+	if (c =='\n')
+		return;
+	if ( c !='\r')
+	{
+		rx_buf[rx_index++]= c;
+		return;
+	}
+	     rx_buf[rx_index] = '\0';
+
+		USART1_echo(rx_buf);
+		rx_index=0;
+	}
+}
+void main(void)
+{
+	IUSART1_Init();
+   RCC->AHB1ENR |= ( 1 << 0 ); // Enable AHB clock for GPIOA
+   RCC->APB2ENR |= ( 1 << 14 ); // Enable USART1 peripheral 
+   GPIOA->PUPDR &= ~(3  << ( 2 * 2 );
+   GPIOA->MODER &= ~( 3 << (2  * 2)); // PA2 as LED
+   GPIOA->MODER |= ( 1 << ( 2 * 2)); // mode is configured as output
+   USART1->CR1 |= USART_CR1_RXNEIE; // enable Receive interrupt for USART1
+	while(1)
+	{
+			}
+}
